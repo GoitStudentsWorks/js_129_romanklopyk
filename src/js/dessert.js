5c7765d8f19074eb36e99eb101339a1aaefb87b9
romankhlopyk <romankhlopyk@outlook.com>
2026-08-13 23:21:05 +0300

    init

---
+++ src/js/dessert.js
@@ -0,0 +1,1 @@
+console.log('dessert.js');
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/api.js';
+import './js/utils.js';
+import './js/bestsellers.js';
+import './js/dessert.js';
+import './js/modal.js';
+import './js/about.js';
+import './js/feedback.js';
+import './js/faq.js';
