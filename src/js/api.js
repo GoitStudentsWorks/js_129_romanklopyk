4ca9b8ad0a9fb88191076337ecddd57608b217d2
romankhlopyk <romankhlopyk@outlook.com>
2026-08-16 00:35:06 +0300

    add js

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,13 @@
-console.log('api.js');
+import axios from 'axios';
+axios.defaults.baseURL = 'https://deserts-store.b.goit.study/api';
+
+async function getData(url, params = {}) {
+    try {
+        const response = await axios(url, params);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default getData;
